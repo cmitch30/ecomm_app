281036c5ce207e81d1e42ac27ab8db4bccd259ca
clark <clark_mitchelljr@hotmail.com>
2023-08-14 11:45:10 -0700

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  return (
-<div className='bg-blue-900 w-screen h-screen flex items-center'>
-  <div className='text-center w-full'>
-    <button className='bg-white p-2 px-4 rounded-lg'>Login With Google</button>
-  </div>
-</div>
-  )
+  return <Layout>Home</Layout>;
 }
